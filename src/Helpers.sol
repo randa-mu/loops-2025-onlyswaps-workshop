a1b0ba278dc47178164f41898e0f669e41ff7a32
Patrick McClurg <patrick.mcclurg@protocol.ai>
2025-09-22 16:56:25 +0200

    Added a bunch of steps, in varying detail

--- src/Helpers.sol
+++ src/Helpers.sol
@@ -1,7 +1,8 @@
 pragma solidity ^0.8.30;
 
 library Helpers {
-  address public constant RUSD_ADDRESS = 0x908e1D85604E0e9e703d52D18f3f3f604Fe7Bb1b;
-  address public constant ROUTER_ADDRESS = 0x3dD1a497846d060Dce130B67b22E1F9DeE18c051;
-  uint256 public constant RECOMMENDED_FEE = 1000000000000000000;
+    address public constant RUSD_ADDRESS = 0x908e1D85604E0e9e703d52D18f3f3f604Fe7Bb1b;
+    address public constant ROUTER_ADDRESS = 0x3dD1a497846d060Dce130B67b22E1F9DeE18c051;
+    uint256 public constant RECOMMENDED_FEE = 1000000000000000000;
+    uint256 public constant AVAX_CHAIN_ID = 43113;
 }
--- src/MyContract.sol
+++ src/MyContract.sol
@@ -1,12 +1,21 @@
 pragma solidity ^0.8.30;
 
-import {IRouter} from "../dependencies/onlyswaps-solidity-v0.1.0/src/interfaces/IRouter.sol";
 import {Helpers} from "./Helpers.sol";
+import {IRouter} from "../dependencies/onlyswaps-solidity-v0.1.0/src/interfaces/IRouter.sol";
+import {ERC20FaucetToken} from "../dependencies/onlyswaps-solidity-v0.1.0/src/mocks/ERC20FaucetToken.sol";
 
 contract MyContract {
     IRouter internal onlyswapsRouter;
+    ERC20FaucetToken internal token;
 
     constructor() {
         onlyswapsRouter = IRouter(Helpers.RUSD_ADDRESS);
+        token = ERC20FaucetToken(Helpers.RUSD_ADDRESS);
+    }
+
+    function spend() public {
+        token.approve(address(this), 1 ether);
+        onlyswapsRouter.getSwapRequestParameters()
+
     }
 }
