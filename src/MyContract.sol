3cbcc9a4c2337aff2c22bea36187652cb92f488f
Patrick McClurg <patrick.mcclurg@protocol.ai>
2025-09-23 09:07:32 +0200

    added comments to the solution

--- src/MyContract.sol
+++ src/MyContract.sol
@@ -1,6 +1,5 @@
 pragma solidity ^0.8.30;
 
-import "../dependencies/onlyswaps-solidity-v0.1.0/src/interfaces/IRouter.sol";
 import {ERC20FaucetToken} from "../dependencies/onlyswaps-solidity-v0.1.0/src/mocks/ERC20FaucetToken.sol";
 import {Helpers} from "./Helpers.sol";
 import {IRouter} from "../dependencies/onlyswaps-solidity-v0.1.0/src/interfaces/IRouter.sol";
@@ -9,7 +8,7 @@ contract MyContract {
     IRouter internal onlyswapsRouter;
     ERC20FaucetToken internal token;
     address internal owner;
-    bytes32 req;
+    bytes32 requestId;
 
     // we need to pass the owner as a constructor arg, because by default forge uses deployer contracts
     // on chains that support it, and using `msg.sender` will set the owner to the deployer!
@@ -25,23 +24,28 @@ contract MyContract {
     function executeSwap() public {
         require(msg.sender == owner, "only the owner can call execute!");
 
-        // 1. you're going to fill in your code to execute a swap here
+        // first we give the router contract the rights to move some tokens for us
         token.approve(Helpers.ROUTER_ADDRESS, 10000 ether);
 
-        req = onlyswapsRouter.requestCrossChainSwap(
+        // then we request the swap itself
+        requestId = onlyswapsRouter.requestCrossChainSwap(
+            // the source and destination token addresses are the same
+            Helpers.RUSD_ADDRESS, 
             Helpers.RUSD_ADDRESS,
-            Helpers.RUSD_ADDRESS,
+            // send 10 RUSD
             10 ether,
-            1 ether,
+            // and suggest a fee of 1USD; the minimum fee accepted by the default solver is 0.01RUSD right now
+            1 ether, 
+            // we're sending it to AVAX
             Helpers.AVAX_CHAIN_ID,
+            // I'm sending it to my own address, but you could also send it to the contract with address(this)
             msg.sender
         );
     }
 
     function hasFinishedExecuting() public view returns (bool) {
-        // 2. you're going to fill in your code to check if it has been verified here
-
-        IRouter.SwapRequestParameters memory params = onlyswapsRouter.getSwapRequestParameters(req);
+        IRouter.SwapRequestParameters memory params = onlyswapsRouter.getSwapRequestParameters(requestId);
+        // this will be true once the solver has verified the swap here
         return params.executed;
     }
 }
